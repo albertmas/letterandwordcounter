bef51c6897b2d489a57709c7c426f2a8ddd64653
albertmas <04011.casp@fje.edu>
2016-11-08 21:50:20 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,12 +6,14 @@ int main()
 {
 	int print1, print2;
 
-	char example[] = "hello world";
+	char example[] = "HeLlO WoRlD eArTh";
 
 	print1 = strlen(example);
 	print2 = wordcount(example);
+	print3 = uppercount(example);
 	printf("There are %d letters\n", print1);
-	printf_s("There are %d words", print2);
+	printf_s("There are %d words\n", print2);
+	printf_s("There are %d upper case letters\n", print3);
 
 	getchar();
 	return 0;
