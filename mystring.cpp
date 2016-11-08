061820f1ce8692805225cce5f83bdb4fd320809b
albertmas <04011.casp@fje.edu>
2016-11-08 21:49:12 +0100

    Update mystring.cpp

--- mystring.cpp
+++ mystring.cpp
@@ -1,7 +1,9 @@
+#include <ctype.h>
+
 typedef unsigned int uint;
 uint strlen(const char* mstr)
 {
-	uint retval = sizeof(mstr) -1;
+	uint retval = sizeof(mstr) - 1;
 
 	return retval;
 }
@@ -10,16 +12,31 @@ uint wordcount(const char* mstr)
 {
 	int i = 0;
 	int words = 1;
-	int* wordpoint = &words;
-	for(i=0; i>sizeof(mstr); ++i)
-	{
-		if (mstr[i] == 32)
-		{
-			++words;
-			++i;
+	char c;
+
+	while (mstr[i]){
+		c = mstr[i];
+		if (isspace(c)){
+			words++;
 		}
+		i++;
 	}
-	
 	return words;
-			
+}
+
+uint uppercount(const char* mstr)
+{
+	int i = 0;
+	int upletters = 0;
+	char c;
+
+	while (mstr[i]){
+		c = mstr[i];
+		if (isupper(c)){
+			upletters++;
+		}
+		i++;
+	}
+
+	return upletters;
 }
